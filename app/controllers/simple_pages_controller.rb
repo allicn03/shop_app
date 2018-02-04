b7bb188392e1a3ebc2a6811d1194ef0300188ecb
allicn03 <a.neusch@gmail.com>
2018-02-04 15:31:47 -0500

    5.3 add contact_form.html.erb set up email template

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -11,9 +11,10 @@ class SimplePagesController < ApplicationController
   @name = params[:name]
   @email = params[:email]
   @message = params[:message]
-  ActionMailer::Base.mail(from: @email,
-      to: 'your-email@example.com',
-      subject: "A new contact form message from #{@name}",
-      body: @message).deliver_now
+  #ActionMailer::Base.mail(from: @email,
+      #to: 'your-email@example.com',
+      #subject: "A new contact form message from #{@name}",
+      #body: @message).deliver_now ( This was prior to adding the UserMailer below)
+  UserMailer.contact_form(@email, @name, @message).deliver_now
   end
 end
