69fde18542a526ffe286fd7abc4ab8ad7e5e0fd5
allicn03 <a.neusch@gmail.com>
2018-07-14 14:35:58 -0400

    5.2 made updates to mailersand search

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,10 @@ class Product < ApplicationRecord
   has_many  :comments
 
   def self.search(search_term)
-    Product.where("name LIKE ?", "%#{search_term}%")
+    if Rails.env.development? #added for search in production
+      Product.where("name LIKE ?", "%#{search_term}%")
+    else #for search in production
+      Product.where("name ilike ?", "%#{search_term}%") #for search in production
   end
 
   def highest_rating_comment
@@ -18,4 +21,5 @@ class Product < ApplicationRecord
   def average_rating
     comments.average(:rating).to_f
   end
+end
 end
