7906dbad0b079eb4089489c3c947a6afdc939003
allicn03 <a.neusch@gmail.com>
2018-09-25 08:55:44 -0400

    updated routes and models to fix signin problem

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,4 @@
-class Order < ApplicationRecord
+class Order < ActiveRecord::Base #prior this line was <ApplicationRecord
   belongs_to :product
   belongs_to :user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,21 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout'}, controllers: { registrations: "user_registrations" }
-  resources :users
-  resources :products do
+  devise_for :users
+    resources :users
+    resources :products do
     resources :comments
   end
+
   resources :orders, only: [:index, :show, :create, :destroy]
-  get 'simple_pages/test'
-  get 'simple_pages/about'
-  get 'simple_pages/contact'
+    get 'simple_pages/test'
+    get 'simple_pages/about'
+    get 'simple_pages/contact'
 
-  get 'simple_pages/landing_page'
+    get 'simple_pages/landing_page'
 
-  post 'simple_pages/thank_you'
+    post 'simple_pages/thank_you'
 
-  root 'simple_pages#index'
-
+    root 'simple_pages#index'
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
+#end
