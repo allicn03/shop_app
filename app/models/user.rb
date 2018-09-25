013d9c90e36593a30464bf3899f231c3e65ea9f2
allicn03 <a.neusch@gmail.com>
2018-09-25 15:59:11 -0400

    move has_many comments and update class name

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
-class User < ActiveRecord::Base#ApplicationRecord
+class User < ApplicationRecord
+  has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable #:trackable
   has_many :orders
-  has_many :comments
 
 end
