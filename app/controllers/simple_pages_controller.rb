e0f18c93efa371016fdac170a3d7eea5e9722beb
allicn03 <a.neusch@gmail.com>
2018-09-25 15:00:01 -0400

    update simple_pages controller commented out actionbase mailer form

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -1,20 +1,21 @@
 class SimplePagesController < ApplicationController
   def index
+    @products = Product.all
   end
+
+      def thank_you
+          @name = params[:name]
+          @email = params[:email]
+          @message = params[:message]
+  #ActionMailer::Base.mail(from: @email,
+  #    to: 'your-email@example.com',
+  #    subject: "A new contact form message from #{@name}",
+  #    body: @message).deliver_now
+          UserMailer.contact_form(@email, @name, @message).deliver_now
+      end
 
   def landing_page
     #@featured_product = Product.first
     @products = Product.limit(3)
-  end
-
-  def thank_you
-  @name = params[:name]
-  @email = params[:email]
-  @message = params[:message]
-  ActionMailer::Base.mail(from: @email,
-      to: 'your-email@example.com',
-      subject: "A new contact form message from #{@name}",
-      body: @message).deliver_now
-  #UserMailer.contact_form(@email, @name, @message).deliver_now
-  end
+  end  
 end
