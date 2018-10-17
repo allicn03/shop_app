91ca01590b5720bea97f0bc816cb7ac9e9a6e9d2
allicn03 <a.neusch@gmail.com>
2018-10-17 19:45:49 -0400

    comment out user to order connections for debugging devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
-class User < ApplicationRecord
+class User < ActiveRecord::Base#ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :orders
-  has_many :comments
+  #has_many :orders
+  #has_many :comments
 
 end
