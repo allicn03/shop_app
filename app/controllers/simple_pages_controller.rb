8499c268971643bafca0c64422ac1ae746200e6d
allicn03 <a.neusch@gmail.com>
2018-10-17 18:59:19 -0400

    make updates to mailers addresses

--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -3,16 +3,18 @@ class SimplePagesController < ApplicationController
     @products = Product.all
   end
 
-      def thank_you
-          @name = params[:name]
-          @email = params[:email]
-          @message = params[:message]
-          ActionMailer::Base.mail(from: @email,
-              to: 'allicn4189@gmail.com',
-              subject: "A new contact form message from #{@name}",
-              body: @message).deliver_now
-          #UserMailer.contact_form(@email, @name, @message).deliver_now
-      end
+  def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
+    #ActionMailer::Base.mail(from: @email,
+    #to: 'allicn4189@gmail.com',
+    #subject: "A new contact form message from #{@name}",
+    #body: @message).deliver_now
+    #UserMailer.contact_form(@email, @name, @message).deliver_now
+    redirect_to root_url, notice: "Thank you. We have received your message and will respond soon."
+  end
 
   def landing_page
     #@featured_product = Product.first
--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'a.neusch@gmail.com'
+  default from: "a.neusch@gmail.com"
   layout 'mailer'
 end
