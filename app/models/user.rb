0545595d13c8163994a124701509400f6a0d7b84
allicn03 <a.neusch@gmail.com>
2018-10-18 10:44:36 -0400

    fixed user superclass problem in users model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 class User < ActiveRecord::Base#ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -6,14 +6,8 @@ class User < ActiveRecord::Base#ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   #has_many :orders
   #has_many :comments
-=======
-class User < ApplicationRecord
+  has_many :orders
   has_many :comments
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable #:trackable
-  has_many :orders
->>>>>>> master
-
 end
